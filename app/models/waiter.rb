0649864eb4d038b392318694cac346465f980d0a
Alex Dunae <alex@dunae.ca>
2015-04-27 16:40:52 -0700

    Remove image code from waiter model

--- app/models/waiter.rb
+++ app/models/waiter.rb
@@ -3,6 +3,9 @@ class Waiter < ActiveRecord::Base
 
 	has_secure_password
 
-	has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+  # This probably doesn't need to be here, right? It was stopping the seeds from running.
+  #
+  #
+	# has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 end
